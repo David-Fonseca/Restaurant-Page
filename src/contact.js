77b78622d6ac72e780af912f41a154b016cb404e
David-Fonseca <davidfonseca56@gmail.com>
2023-06-13 19:15:17 -0600

    small fix to map

--- src/contact.js
+++ src/contact.js
@@ -34,7 +34,7 @@ function displayContact(){
                 </div>
                 
             </form>
-            <p align='center'>
+            <p align='center' class='if'>
                 <iframe class='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.6703923613136!2d-74.05868152500898!3d4.652742995322127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a424bc107b5%3A0xb152a5554d1ebb05!2sCra.%207%20%2370-24%2C%20Localidad%20de%20Chapinero%2C%20Bogot%C3%A1%2C%20Colombia!5e0!3m2!1sen!2sca!4v1686703871834!5m2!1sen!2sca" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </p>
         </div>
